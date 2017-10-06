1a829678c15567ea9a70aeb2d342c70c2a725ae3
Murugan <murugu2012@gmail.com>
2017-10-06 08:36:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+formathtmlerb
+===================
+
+This plugins adds a new html erb formatter to C9 based on htmlbeautifier.
+
+Requirements
+------------
+
+ * gem install htmlbeautifier
+
+
+Installation on c9.io
+---------------------
+
+Run the following in c9 terminal:
+
+    # Create a folder
+    mkdir ~/.c9/plugins
+
+    # Clone the plugin
+    git clone https://github.com/firstdraft/formathtmlerb.git ~/.c9/plugins/formathtmlerb
+
+    # Click Cloud9 -> Open Your Init Script and add the below code
+    services.pluginManager.loadPackage([
+      "~/.c9/plugins/formathtmlerb/package.json",
+    ])
+
+    # Refresh the page
+
+License
+-------
+
+formathtmlerb is licensed under the AGPL version 3
---
+++ formathtmlerb.js
@@ -0,0 +1,59 @@
+define(function(require, exports, module) {
+
+    main.consumes = ["Plugin", "settings", "preferences", "format", "proc", "c9", "tabManager", "save",
+        "error_handler", "dialog.error"
+    ];
+    main.provides = ["formathtmlerb"];
+
+
+    return main;
+
+    function main(options, imports, register) {
+        var Plugin = imports.Plugin;
+
+        var proc = imports.proc;
+        var tabManager = imports.tabManager;
+        var c9 = imports.c9;
+
+        var plugin = new Plugin("Murugan", main.consumes);
+        var format = imports.format;
+
+        function load() {
+            format.on("format", function(e) {
+                if (e.mode == "html_ruby") {
+                    return formatCode(e.editor, e.mode);
+                }
+            });
+        }
+
+        plugin.on("load", function() {
+            load();
+        });
+        plugin.on("unload", function() {
+
+        });
+
+        /***** Register and define API *****/
+
+        function formatCode(editor) {
+            var path = c9.workspaceDir + tabManager.focussedTab.path;
+
+            proc.execFile("htmlbeautifier", {
+                args: [path],
+                cwd: '/home/ubuntu/workspace'
+            }, function(err, stdout, stderr) {
+
+            });
+
+            return true;
+        }
+
+        plugin.freezePublicAPI({
+            formatCode: formatCode,
+        });
+
+        register(null, {
+            "formathtmlerb": plugin
+        });
+    }
+});
